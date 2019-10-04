735f5bf3103bd099405c11c5a8763a0e3ae04d24
stilesma <akinstiles349@gmail.com>
2019-10-04 10:29:16 +0100

    error in server.js corrected

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
 const localCSP = csp.getCSP(cspPolicy);
 
 app.use(globalCSP);
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(passport.initialize())
-app.use(express.static(__dirname + '/public'))
+
 
 require('./config/passport')(passport)
 
@@ -40,5 +41,12 @@ mongoose.connect(
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 const port = process.env.PORT || 5000; 
+if(process.env.NODE_ENV  === 'production')
+{
+  app.use(express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
 
