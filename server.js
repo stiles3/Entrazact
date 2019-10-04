b17b5db2ede8748ebd3552d0c582b36508ded04f
stilesma <akinstiles349@gmail.com>
2019-10-04 10:42:39 +0100

    csp removed finally

--- server.js
+++ server.js
@@ -3,24 +3,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require("passport");
-const csp = require('content-security-policy');
+
 
 const users = require("./routes/api/users");
 
 const app = express()
 const cors = require('cors')
 
-const cspPolicy = {
-  'report-uri': '/reporting',
-  'default-src': csp.SRC_SELF,
-  'img-src': csp.SRC_SELF,
-  'script-src': [ csp.SRC_SELF, csp.SRC_DATA ]
-};
 
-const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
-const localCSP = csp.getCSP(cspPolicy);
-
-app.use(globalCSP);
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 app.use(bodyParser.json())
