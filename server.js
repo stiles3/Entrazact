7894019c5ad50713ceca2cf88ff38eb9729b0b27
stilesma <akinstiles349@gmail.com>
2019-10-04 10:06:50 +0100

    csp readded

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require("passport");
+const csp = require('content-security-policy');
 
 const users = require("./routes/api/users");
 
 const app = express()
 const cors = require('cors')
 
+const cspPolicy = {
+  'report-uri': '/reporting',
+  'default-src': csp.SRC_SELF,
+  'img-src': csp.SRC_SELF,
+  'script-src': [ csp.SRC_SELF, csp.SRC_DATA ]
+};
+
+const globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
+const localCSP = csp.getCSP(cspPolicy);
+
+app.use(globalCSP);
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cors())
