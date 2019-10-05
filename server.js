83ce08eb525a53fafe455023671bd2b2c40df0a4
stilesma <akinstiles349@gmail.com>
2019-10-05 08:36:33 +0100

    path added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require("passport");
-
+const path = require('path')
 
 const users = require("./routes/api/users");
 
